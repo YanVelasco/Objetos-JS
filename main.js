b2382462b833ad6a1a70637dc91ca20ece2d76ff
Yan_Velasco <yancarlosduartevelasco@gmail.com>
2024-02-18 15:51:48 -0300

    Editando Objetos na DOM

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let listaItens = [];
+let itemAEditar;
 
 let form = document.getElementById("form-itens");
 const itensInput = document.querySelector("#receber-item");
@@ -49,16 +50,18 @@ function mostrarItem() {
       `;
     } else {
       ulItens.innerHTML += `
-          <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
-              <div>
-                  <input type="checkbox" class="is-clickable" />
-                  <input type="text" class="is-size-5" value="${elemento.item}"></input>
-              </div>
-              <div>
-                  <i class="fa-solid fa-trash is-clickable deletar"></i>
-              </div>
-          </li>
-          `;
+            <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                <div>
+                    <input type="checkbox" class="is-clickable" />
+                    <input type="text" class="is-size-5" value="${elemento.item}" ${index !== Number(itemAEditar) ? 'disabled' : ''}></input>
+                </div>
+                <div>
+                    ${ index === Number(itemAEditar) ? '<button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button>' : '<i class="fa-regular is-clickable fa-pen-to-square editar"></i>'}
+                    <i class="fa-solid fa-trash is-clickable deletar"></i>
+                </div>
+            </li>
+`;
+
     }
     itensInput.value = "";
   });
@@ -83,4 +86,20 @@ function mostrarItem() {
       mostrarItem();
     });
   });
+
+  const editarItens = document.querySelectorAll('.editar');
+  editarItens.forEach((i) => {
+    i.addEventListener("click", (event) => {
+      itemAEditar =
+        event.target.parentElement.parentElement.getAttribute("data-value");
+      mostrarItem();
+    });
+  });
+}
+
+function salvarEdicao() {
+  const itemEditado = document.querySelector(`[data-value="${itemAEditar}"] input[type="text"]`)
+  listaItens[itemAEditar].item = itemEditado.value;
+  itemAEditar = -1;
+  mostrarItem();
 }

d62232beb046f759e35009a17cf5293a49c187a8
Yan_Velasco <yancarlosduartevelasco@gmail.com>
2024-02-18 11:51:17 -0300

    Manipulando elementos na DOM

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let listaItens = [];
 let form = document.getElementById("form-itens");
 const itensInput = document.querySelector("#receber-item");
 const ulItens = document.querySelector("#lista-de-itens");
+const ulItensComprados = document.querySelector("#itens-comprados");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -21,6 +22,7 @@ function salvarItem() {
   } else {
     listaItens.push({
       item: comprasItem,
+      checar: false,
     });
 
     console.log(listaItens);
@@ -29,17 +31,44 @@ function salvarItem() {
 
 function mostrarItem() {
   ulItens.innerHTML = "";
+  ulItensComprados.innerHTML = "";
+
   listaItens.forEach((elemento, index) => {
-    ulItens.innerHTML += `
-        <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+    if (elemento.checar) {
+      ulItensComprados.innerHTML += `
+          <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
             <div>
-                <input type="checkbox" class="is-clickable" />
-                <input type="text" class="is-size-5" value="${elemento.item}"></input>
+                <input type="checkbox" checked class="is-clickable" />
+                <span class="itens-comprados is-size-5">${elemento.item}</span>
             </div>
             <div>
                 <i class="fa-solid fa-trash is-clickable deletar"></i>
             </div>
         </li>
-        `;
+      `;
+    } else {
+      ulItens.innerHTML += `
+          <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+              <div>
+                  <input type="checkbox" class="is-clickable" />
+                  <input type="text" class="is-size-5" value="${elemento.item}"></input>
+              </div>
+              <div>
+                  <i class="fa-solid fa-trash is-clickable deletar"></i>
+              </div>
+          </li>
+          `;
+    }
+  });
+
+  const inputCheck = document.querySelectorAll('input[type="checkbox"]');
+
+  inputCheck.forEach((i) => {
+    i.addEventListener("click", (event) => {
+      const valorDoElemento =
+        event.target.parentElement.parentElement.getAttribute("data-value");
+      listaItens[valorDoElemento].checar = event.target.checked;
+      mostrarItem();
+    });
   });
 }
